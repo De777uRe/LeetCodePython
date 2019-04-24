6d2632f9543fef42c52438293ea2bee49bbdc504
William Goins <de777urest@gmail.com>
2019-04-24 00:00:43 -0500

    Closer

--- OutermostParentheses.py
+++ OutermostParentheses.py
@@ -1,20 +1,25 @@
 class Solution:
     def removeOuterParentheses(self, S: str) -> str:
         result = ""
-        indices = []
-        for i in range(len(S)-1):
-            if S[i] == S[i+1]:
-                if S[i] == '(':
-                    indices.append(i)
-                else:
-                    indices.append(i+1)
-
-        for j in range(len(indices)-1):
-            result += S[indices[j]+1:indices[j+1]]
+        substr = ""
+        index = 0
+        while index < len(S)-1:
+            if S[index] == S[index+1] and S[index] == '(':
+                for j in range(index+1, len(S)-1, 1):
+                    if S[j] == ')' and S[j+1] == ')':
+                        substr += S[j]
+                        index = j
+                        break
+                    else:
+                        substr += S[j]
+                result += substr
+                substr = ""
+            index += 1
 
         return result
 
 
 solutionObj = Solution()
+# expected = solutionObj.removeOuterParentheses("(()())(())")
 expected = solutionObj.removeOuterParentheses("(()())(())(()(()))")
 print(expected)
